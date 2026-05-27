c74ffe843796f9344387d8ed46da81d08aa445f8
ta0ma0 <exemple@exemple.com>
2026-05-27 03:47:50 +0300

    security: move DJANGO_SECRET_KEY to .env, remove git fallback

--- django-aws-terminal-websocket/vmwebsocket/settings.py
+++ django-aws-terminal-websocket/vmwebsocket/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "django-insecure-6pjemd9*ot-oxey76n%wk-#9t1qekvs0$^bypomln%+&hy)l+5")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DEBUG = os.getenv("DJANGO_DEBUG", "0") == "1"
 
